e78c2a721c70c7dd92abb7114e3f683313bfa17a
Vyom Thaker <vyomthaker21@gmail.com>
2024-09-24 10:18:49 +0530

    ifelse

---
+++ Basics/ifelse.java
@@ -0,0 +1,15 @@
+package Basics;
+import java.util.*;
+
+public class ifelse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+    }
+}
