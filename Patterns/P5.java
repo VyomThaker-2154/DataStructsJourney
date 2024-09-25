92f1a43823faf22c743e5df6d171ea1ab7d730bf
Vyom Thaker <vyomthaker21@gmail.com>
2024-09-25 11:07:36 +0530

    P5

---
+++ Patterns/P5.java
@@ -0,0 +1,17 @@
+package Patterns;
+import java.util.*;
+
+public class P5 {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner (System.in)) {
+            int n = scanner.nextInt();
+            
+            for(int i=n ; i > 0 ; i--){
+                for(int j=1 ; j <=i ; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
