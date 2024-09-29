ba06d05fa360eed2955e49d455688f6f57951d28
Vyom Thaker <vyomthaker21@gmail.com>
2024-09-29 20:03:52 +0530

    Palindrome

--- BasicMaths/palindrome.java
+++ BasicMaths/palindrome.java
@@ -1,5 +1,31 @@
 package BasicMaths;
 
 public class palindrome {
-    
+    static boolean palindrom(int n) {
+        int revNum = 0;
+        int dup = n;
+        while (n > 0) {
+            int ld = n % 10;
+            revNum = (revNum * 10) + ld;
+            n = n / 10;
+        }
+        if (dup == revNum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 4554;
+
+        if (palindrom(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
 }
+                            
+                        
+    
